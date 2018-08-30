4aa739f265223b4b28f2669dcf87fad767248339
hoodielive <hoodielive@gmail.com>
2018-08-29 20:13:36 -0400

    space-complexity

---
+++ javascript/big-O-notation/space-comp.js
@@ -0,0 +1,15 @@
+
+function sum(arr) {
+	let total = 0; 
+	for (let i = 0; i < arr.length; i++) {
+		total += arr[i];
+	} 
+	return total; 
+}
+
+function double(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		newArr.push(2 * arr[i]); 
+		}
+		return newArr; 
+}
