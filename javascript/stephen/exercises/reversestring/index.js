82a52b4b32daba9b4a4e5ba97cb6566b8c3f6d1d
hoodielive <hoodielive@gmail.com>
2018-09-12 00:19:36 -0400

    debugger on reversed strings

--- javascript/stephen/exercises/reversestring/index.js
+++ javascript/stephen/exercises/reversestring/index.js
@@ -7,7 +7,9 @@ function reverse(str) {
 
 
 function reversed(str) {
-  debugger; 
+  debugger; // make sure to invoke the function and then run with node inspect index.js | enter repl mode and cont
   return str.split('').reduce((rev, char) => char + rev, ''); 
 }
+
+reversed('asdf')
 module.exports = reverse; 
