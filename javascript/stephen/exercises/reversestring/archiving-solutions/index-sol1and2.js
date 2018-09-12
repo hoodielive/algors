31fb6e1723fa10c07f0dc5016e8e2d0d0f80b1fc
hoodielive <hoodielive@gmail.com>
2018-09-12 00:07:18 -0400

    string reversal

---
+++ javascript/stephen/exercises/reversestring/archiving-solutions/index-sol1and2.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+//function reverse(str) {
+//  const arr = str.split(''); 
+//  arr.reverse(); 
+//  return arr.join('');
+//}
+
+//reverse('larry')
+
+//#function reverser(str2) {
+//  return str.split('').reverse().join('')
+//}
+
+//reverser('law')
+
+function reverse(str) {
+  let reversed = ''; 
+
+  for (let character of str) {
+    reversed = character + reversed;  
+  }
+  
+  return reversed; 
+}
+
+module.exports = reverse;
--- javascript/stephen/exercises/reversestring/index.js
+++ javascript/stephen/exercises/reversestring/index.js
@@ -1,11 +1,9 @@
-// --- Directions
-// Given a string, return a new string with the reversed
-// order of characters
-// --- Examples
-//   reverse('apple') === 'leppa'
-//   reverse('hello') === 'olleh'
-//   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) { 
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;   
+  }, '');
+}
 
-module.exports = reverse;
+
+module.exports = reverse; 
