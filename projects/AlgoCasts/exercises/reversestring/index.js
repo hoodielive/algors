3b45fff9190f4d8c8bb55e7d8c072dd88f5b0e03
hoodielive <hoodielive@gmail.com>
2018-12-01 19:49:29 -0500

    reverse a string

--- projects/AlgoCasts/exercises/reversestring/index.js
+++ projects/AlgoCasts/exercises/reversestring/index.js
@@ -5,7 +5,19 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+    // let stringArray = str.split(""); // stringArray = stringArray.reverse() // return stringArray.join(""); 
+    // or 
+    // let reversed = ''; 
+    // for (let character of str ) {
+    //   reversed = character + reversed; 
+    // }
+    // return reversed; 
 
-function reverse(str) {}
+
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, ''); 
+};
+
+reverse("apple");
 
 module.exports = reverse;
